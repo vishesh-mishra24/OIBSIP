afae64989533890097dbec383d69c078981fed48
vishesh-mishra24 <130219037+vishesh-mishra24@users.noreply.github.com>
2023-04-28 21:55:23 +0530

    Add files via upload

---
+++ portfolio/responsive-portfolio/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Vishesh Mishra</span> I am a web developer and designer.I love to make responsive websites based on html.css,javascript,react.I gave more time in solving DSA problems.My goal is to become a good software developer. "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
